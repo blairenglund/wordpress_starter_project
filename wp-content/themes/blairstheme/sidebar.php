52965a4c9e4db9e72a1231b376a8239d02b2c899
Blair Englund <blair.englund@gmail.com>
2016-08-08 15:17:35 -0500

    playing with some more wordpress features

--- wp-content/themes/blairstheme/sidebar.php
+++ wp-content/themes/blairstheme/sidebar.php
@@ -14,21 +14,13 @@
 		</form>
 		<h3>Pages</h3>
 		<ul>
-		<?php 
-			$pages = array("home"=> "index.php", "goals"=> "goals.php", "photos"=> "photos.php");
-			foreach ($pages as $page => $address) {
-				if ($page == $pageID) {
-					echo "<li id='currentpage'><a href=".$address.">".$page."</a></li>";
-				}
-				else { 
-					echo "<li><a href=".$address.">".$page."</a></li>";
-				}
-			}
-		?>
+			<?php wp_list_pages('title_li=') ?>
 		</ul>
 		<h3>Projects</h3>
-		<?php include 'php/listprojects.php' ?>
+			<?php include 'php/listprojects.php' ?>
 		<h3>Articles</h3>
-		<?php include 'php/listarticles.php' ?>
+		<ul>
+			<?php wp_get_archives( array('type'=>'postbypost') ); ?>
+		</ul>
 	</div>
 </div>
