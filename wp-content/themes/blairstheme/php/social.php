d12056d3e495883e9e83ce09e19d108f6718c07a
Blair Englund <blair.englund@gmail.com>
2016-08-09 09:56:28 -0500

    got rid of extraneous files

--- wp-content/themes/blairstheme/php/social.php
+++
@@ -1,6 +0,0 @@
-<div class="social">
-	<?php 
-		$links = array("icons/icon_twitter_white.png"=> "https://twitter.com/blairenglund", "icons/icon_linkedin_white.png"=> "https://www.linkedin.com/in/blairenglund", "icons/icon_git_white.png"=> "https://github.com/blairenglund");
-		foreach ($links as $img => $address) {echo "&thinsp; <a href=".$address."><img src=".$img." height=35 ></a> &thinsp;";}
-	?>
-</div>
